20df5dc96b23b8c40aef60bac23053c27ea391d5
Vahe Odabashyan <vaheodabashyan@gmail.com>
2019-12-23 01:59:29 +0400

    a simple chat appliaction - draft version

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our interactive chat!!!");
+        System.out.print("Please enter the number of chat members: ");
+
+        Scanner in = new Scanner(System.in);
+        int chatMembers = in.nextInt();
+
+        ArrayList<Member> memberList = new ArrayList<>();
+        for (int i = 0; i < chatMembers; i++) {
+            memberList.add(new Member(i));
+        }
+
+        int command;
+        String message;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter 1 to send a message or 2 to exit: ");
+
+
+        while (!memberList.isEmpty()) {
+            message = s.nextLine();
+
+            if (message.equals("2")) {
+                System.out.println("A member left the chat!");
+                memberList.remove(0);
+            } else if (message.equals("1")) {
+                System.out.println("Type a message!");
+                continue;
+            } else {
+                Random r = new Random();
+                Member member = memberList.get(r.nextInt(memberList.size()));
+                System.out.println(member + message);
+            }
+        }
+    }
+}
+
+
---
+++ src/Member.java
@@ -0,0 +1,33 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Member {
+    private int id;
+    private boolean isActive = true;
+
+    public Member(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id + 1;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        String sysTime = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+        return sysTime + " member" + getId() + ": ";
+    }
+}
